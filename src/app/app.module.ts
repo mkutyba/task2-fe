f290f26713874c1716311ba8fba1f5f63d77277c
Mateusz Kutyba <mateusz.kutyba@gmail.com>
2017-09-08 08:07:04 +0200

    Install Angular Material and related packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialComponentsModule} from './material-components.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialComponentsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
